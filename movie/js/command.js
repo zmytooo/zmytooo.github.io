3f222eead2e9220afafb1feee128f3b7b607d238
Jasonellen <lanou@YF-Jason.local>
2016-12-10 20:00:24 +0800

    gb

--- movie/js/command.js
+++ movie/js/command.js
@@ -4,35 +4,59 @@
 //     $(this).find("img").attr('src','../img/movie.png');   
 // });
 
-$(".nav ul li").eq(2).mouseover(function() {
-    $(this).find("img").attr('src','../img/tv2.png');   
-  }).mouseout(function(){
-    $(this).find("img").attr('src','../img/tv.png');   
-});
-
-$(".nav ul li").eq(3).mouseover(function() {
-    $(this).find("img").attr('src','../img/show2.png');   
-  }).mouseout(function(){
-    $(this).find("img").attr('src','../img/show.png');   
-});
-
-$(".nav ul li").eq(4).mouseover(function() {
-    $(this).find("img").attr('src','../img/cartoon2.png');   
-  }).mouseout(function(){
-    $(this).find("img").attr('src','../img/cartoon.png');   
-});
-
-$(".nav ul li").eq(5).mouseover(function() {
-    $(this).find("img").attr('src','../img/all2.png');   
-  }).mouseout(function(){
-    $(this).find("img").attr('src','../img/all.png');   
-});
-
-
 var li1 = document.querySelector('.nav ul li:nth-child(2)');
 var pic1 = document.querySelector('.nav ul li:nth-child(2) img');
 li1.onmouseover=function(){pic1.src="../img/movie2.png";}
 li1.onmouseout=function(){pic1.src="../img/movie.png";}
+
+var li2 = document.querySelector('.nav ul li:nth-child(3)');
+var pic2 = document.querySelector('.nav ul li:nth-child(3) img');
+li2.onmouseover=function(){pic2.src="../img/tv2.png";}
+li2.onmouseout=function(){pic2.src="../img/tv.png";}
+
+var li3 = document.querySelector('.nav ul li:nth-child(4)');
+var pic3 = document.querySelector('.nav ul li:nth-child(4) img');
+li3.onmouseover=function(){pic3.src="../img/show2.png";}
+li3.onmouseout=function(){pic3.src="../img/show.png";}
+
+var li4 = document.querySelector('.nav ul li:nth-child(5)');
+var pic4 = document.querySelector('.nav ul li:nth-child(5) img');
+li4.onmouseover=function(){pic4.src="../img/cartoon2.png";}
+li4.onmouseout=function(){pic4.src="../img/cartoon.png";}
+
+var li5 = document.querySelector('.nav ul li:nth-child(6)');
+var pic5 = document.querySelector('.nav ul li:nth-child(6) img');
+li5.onmouseover=function(){pic5.src="../img/all2.png";}
+li5.onmouseout=function(){pic5.src="../img/all.png";}
+
+
+
+// $(".nav ul li").eq(2).mouseover(function() {
+//     $(this).find("img").attr('src','../img/tv2.png');   
+//   }).mouseout(function(){
+//     $(this).find("img").attr('src','../img/tv.png');   
+// });
+
+// $(".nav ul li").eq(3).mouseover(function() {
+//     $(this).find("img").attr('src','../img/show2.png');   
+//   }).mouseout(function(){
+//     $(this).find("img").attr('src','../img/show.png');   
+// });
+
+// $(".nav ul li").eq(4).mouseover(function() {
+//     $(this).find("img").attr('src','../img/cartoon2.png');   
+//   }).mouseout(function(){
+//     $(this).find("img").attr('src','../img/cartoon.png');   
+// });
+
+// $(".nav ul li").eq(5).mouseover(function() {
+//     $(this).find("img").attr('src','../img/all2.png');   
+//   }).mouseout(function(){
+//     $(this).find("img").attr('src','../img/all.png');   
+// });
+
+
+
 
 
 
